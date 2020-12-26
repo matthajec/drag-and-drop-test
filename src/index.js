d9ba92a3232a068c0ee7d16cc3761f6f70d32639
matthajec <66030008+matthajec@users.noreply.github.com>
2020-12-26 18:33:35 -0500

    added files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import '@atlaskit/css-reset'
+import { DragDropContext } from 'react-beautiful-dnd'
+import initialData from './initial-data'
+import Column from './column'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [data, setData] = useState(initialData)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const onDragEnd = result => {
+    const { destination, source, draggableId } = result
+
+    if (!destination) { // if there is no destination end the execution
+      return
+    }
+
+    if ( // if the droppable and the index are the same the item was dropped into the position it started in, end the exection
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = data.columns[source.droppableId]
+    const newTasksIds = Array.from(column.taskIds) // 'array.from' makes sure that the state isn't passed by reference to avoid directly modifying state 
+    newTasksIds.splice(source.index, 1) // remove the moved draggable from tasks
+    newTasksIds.splice(destination.index, 0, draggableId) // add the moved draggable back but in it's new position
+
+
+    const newColumn = {
+      ...column,
+      taskIds: newTasksIds
+    }
+
+    setData({
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn
+      }
+    })
+  }
+
+  return (
+    <DragDropContext
+      onDragEnd={onDragEnd}
+    >
+      {data.columnOrder.map((columnId) => {
+        const column = data.columns[columnId]
+        const tasks = column.taskIds.map(taskId => data.tasks[taskId])
+
+        return (
+          <Column key={column.id} column={column} tasks={tasks} />
+        )
+      })}
+    </DragDropContext>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/task.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Draggable } from 'react-beautiful-dnd'
+
+const Container = styled.div`
+  border: 1px solid lightgray;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: #fff;
+`
+
+export default function Task({ task, index }) {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <Container
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {task.content}
+        </Container>
+      )}
+    </Draggable>
+  )
+}
